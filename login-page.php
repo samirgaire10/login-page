b9041b7dc6cc75b3699b9b95bcf34aad73e75a2c
samir gaire <118608337+samirgaire10@users.noreply.github.com>
2022-12-18 08:38:17 +0900

    login-php

---
+++ login-page.php
@@ -0,0 +1,55 @@
+ 
+<!DOCTYPE html>
+<html>
+
+   <head>
+
+   <title>practice</title>
+
+   </head>
+
+   <body>
+
+<form>
+ 名前:  <input type="name" name="pass" > <br><br>
+ パスワード:  <input type="password" name="pass" > <br><br>
+
+ <h5>性別</h5>
+<input type="radio" name="sex" value="0"  checked >男..
+<input type="radio" name="sex" value="1">女..
+<input type="radio" name="sex" value="3">わからん..
+
+<br>
+<br>
+
+ <select size="4" name="pref">
+ <option vlaue="1">日本</option>
+ <option vlaue="2">ネパール</option>
+ <option vlaue="3">アメリカ</option>
+ <option vlaue="4">インド</option>
+ <option vlaue="5">韓国</option>
+ <option vlaue="6">北朝鮮 </option>
+ <option vlaue="7">宇宙人</option>
+ <option vlaue="7">わからん？</option>
+
+</select>
+
+
+
+<br>
+<br>
+
+   <input type="submit" name="btn" value="送信">
+   <input type="reset" value="書き直し">
+
+
+   <br><br>
+   <a href="./index.html">skip<a>
+
+</form>
+
+
+
+
+   </body>
+   </html>
