6dfa5a6fba5c91a7581c302ddb54f1a4583517e9
samir gaire <118608337+samirgaire10@users.noreply.github.com>
2022-12-18 08:46:53 +0900

    In english

--- login-page.php
+++ login-page.php
@@ -11,26 +11,27 @@
    <body>
 
 <form>
- 名前:  <input type="name" name="pass" > <br><br>
- パスワード:  <input type="password" name="pass" > <br><br>
+ First Name:  <input type="name" name="first_name_of_user" >    Last Name:  <input type="name" name="Last_name_of_user" > <br><br>
+ Password:  <input type="password" name="pass" > <br><br>
 
- <h5>性別</h5>
-<input type="radio" name="sex" value="0"  checked >男..
-<input type="radio" name="sex" value="1">女..
-<input type="radio" name="sex" value="3">わからん..
+ <h5>Gender</h5>
+<input type="radio" name="sex" value="0" checked >Boy..
+<input type="radio" name="sex" value="1">Girl..
+<input type="radio" name="sex" value="3">Rather not say..
 
 <br>
 <br>
 
  <select size="4" name="pref">
- <option vlaue="1">日本</option>
- <option vlaue="2">ネパール</option>
- <option vlaue="3">アメリカ</option>
- <option vlaue="4">インド</option>
- <option vlaue="5">韓国</option>
- <option vlaue="6">北朝鮮 </option>
- <option vlaue="7">宇宙人</option>
- <option vlaue="7">わからん？</option>
+ <option vlaue="1">US</option>
+ <option vlaue="1">Japan</option>
+  <option vlaue="2">Nepal</option>
+ <option vlaue="3">Korea</option>
+ <option vlaue="4">India</option>
+ <option vlaue="5">China</option>
+ <option vlaue="6">North Korea</option>
+ <option vlaue="7">Alien</option>
+ <option vlaue="7">Rather not say</option>
 
 </select>
 
@@ -40,7 +41,7 @@
 <br>
 
    <input type="submit" name="btn" value="送信">
-   <input type="reset" value="書き直し">
+   <input type="reset" value="Submit">
 
 
    <br><br>
